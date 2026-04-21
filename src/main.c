2b4298f636c7022b0e02516a73dcf6d85dbaee88
lordisfet <lordisfet@gmail.com>
2026-04-21 18:52:47 +0300

    Realease switch with hardware timer

--- src/main.c
+++ src/main.c
@@ -3,26 +3,45 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 
-static const char *TAG = "BLINK";
+static const char *TAG_LAMP = "BLINK";
 const int LAMP_CONTROL_PIN = 4;
+const unsigned long timeON = 1000000;
+const unsigned long timeOFF = 5000000;
+
+bool lampState = false;
+esp_timer_handle_t lamp_timer_hendler;
+
+void switchLampState()
+{
+    lampState = !lampState;
+    gpio_set_level(LAMP_CONTROL_PIN, lampState);
+
+    if (lampState)
+    {
+        ESP_ERROR_CHECK(esp_timer_start_once(lamp_timer_hendler, timeON));
+    }
+    else
+    {
+        ESP_ERROR_CHECK(esp_timer_start_once(lamp_timer_hendler, timeOFF));
+    }
+
+    ESP_LOGI(TAG_LAMP, "Lamp state changed to %d", lampState);
+}
 
 void app_main()
 {
-    ESP_LOGI(TAG, "app_main initial");
-
     gpio_reset_pin(LAMP_CONTROL_PIN);
     gpio_set_direction(LAMP_CONTROL_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LAMP_CONTROL_PIN, lampState);
 
-    bool ledState = false;
+    const esp_timer_create_args_t lamp_timer_args = {
+        .callback = switchLampState,
+        .name = "lamp_timer",
+    };
 
-    // ESP_LOGI(TAG, "start main cycle");
+    ESP_ERROR_CHECK(esp_timer_create(&lamp_timer_args, &lamp_timer_hendler));
 
-    while (1)
-    {
-        ledState = !ledState;
-        gpio_set_level(LAMP_CONTROL_PIN, ledState);
-        ESP_LOGI(TAG, "swirch led state to %d", ledState);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    ESP_ERROR_CHECK(esp_timer_start_once(lamp_timer_hendler, timeOFF));
 }
