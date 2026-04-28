1f44d42b9d6317df29f3469c5d21fc259764cdae
lordisfet <lordisfet@gmail.com>
2026-04-28 07:34:58 +0300

    split class to .h and .cpp files

--- lib/myButton/myButton.h
+++ lib/myButton/myButton.h
@@ -43,27 +43,6 @@ private:
     int _lastlevel;
     uint64_t _lastlevelSwitchTime;
 
-    esp_err_t init()
-    {
-        const gpio_config_t configButton =
-            {
-                .pin_bit_mask = 1ULL << _pin,
-                .mode = GPIO_MODE_INPUT,
-                .pull_up_en = _PULL_MODE == PULL_UP ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE,
-                .pull_down_en = _PULL_MODE == PULL_DOWN ? GPIO_PULLDOWN_ENABLE : GPIO_PULLDOWN_DISABLE,
-                .intr_type = GPIO_INTR_DISABLE};
-
-        esp_err_t err = gpio_config(&configButton);
-        if (err != ESP_OK)
-        {
-            ESP_LOGE(_TAG_BUTTON, "Failed to configure GPIO: %d", err);
-            return err;
-        }
-
-        _lastlevel = gpio_get_level(_pin);
-        return ESP_OK;
-    }
-
 public:
     myButton(const char *tag, gpio_num_t pin, const enum ButtonActiveLevel activeLevel = ACTIVE_LOW, const enum ButtonPullMode pullMode = PULL_UP,
              void (*onPress)(void) = nullptr)
@@ -73,52 +52,6 @@ public:
         _lastlevelSwitchTime = 0;
     };
 
-    enum ButtonState button_update()
-    {
-        enum ButtonState stateToReturn = _internallState;
-        int currentLevel = gpio_get_level(_pin);
-        uint64_t currentTime = esp_timer_get_time();
-
-        switch (_internallState)
-        {
-        case IDLE:
-            if (currentLevel != _lastlevel)
-            {
-                _lastlevelSwitchTime = currentTime;
-                _internallState = DEBOUNCE;
-            }
-            break;
-        case DEBOUNCE:
-            if (currentTime - _lastlevelSwitchTime >= _DEBOUNCE_TIME_US && currentLevel != _lastlevel)
-            {
-                if (currentLevel == _ACTIVE_LEVEL)
-                {
-                    _internallState = PRESSED;
-                }
-                else
-                {
-                    _internallState = RELEASED;
-                }
-            }
-            break;
-        case PRESSED:
-            if (_onPress != nullptr)
-            {
-                _onPress();
-            }
-
-            _lastlevel = currentLevel;
-            _internallState = IDLE;
-            break;
-        case RELEASED:
-
-            _lastlevel = currentLevel;
-            _internallState = IDLE;
-            break;
-        default:
-            break;
-        }
-
-        return stateToReturn;
-    }
+    esp_err_t init();
+    enum ButtonState button_update();
 };
---
+++ lib/myButton/myButtton.cpp
@@ -0,0 +1,71 @@
+#include "myButton.h"
+
+esp_err_t myButton::init()
+{
+    const gpio_config_t configButton =
+        {
+            .pin_bit_mask = 1ULL << _pin,
+            .mode = GPIO_MODE_INPUT,
+            .pull_up_en = _PULL_MODE == PULL_UP ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE,
+            .pull_down_en = _PULL_MODE == PULL_DOWN ? GPIO_PULLDOWN_ENABLE : GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE};
+
+    esp_err_t err = gpio_config(&configButton);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(_TAG_BUTTON, "Failed to configure GPIO: %d", err);
+        return err;
+    }
+
+    _lastlevel = gpio_get_level(_pin);
+    return ESP_OK;
+}
+
+enum ButtonState myButton::button_update()
+{
+    enum ButtonState stateToReturn = _internallState;
+    int currentLevel = gpio_get_level(_pin);
+    uint64_t currentTime = esp_timer_get_time();
+
+    switch (_internallState)
+    {
+    case IDLE:
+        if (currentLevel != _lastlevel)
+        {
+            _lastlevelSwitchTime = currentTime;
+            _internallState = DEBOUNCE;
+        }
+        break;
+    case DEBOUNCE:
+        if (currentTime - _lastlevelSwitchTime >= _DEBOUNCE_TIME_US && currentLevel != _lastlevel)
+        {
+            if (currentLevel == _ACTIVE_LEVEL)
+            {
+                _internallState = PRESSED;
+            }
+            else
+            {
+                _internallState = RELEASED;
+            }
+        }
+        break;
+    case PRESSED:
+        if (_onPress != nullptr)
+        {
+            _onPress();
+        }
+
+        _lastlevel = currentLevel;
+        _internallState = IDLE;
+        break;
+    case RELEASED:
+
+        _lastlevel = currentLevel;
+        _internallState = IDLE;
+        break;
+    default:
+        break;
+    }
+
+    return stateToReturn;
+}
