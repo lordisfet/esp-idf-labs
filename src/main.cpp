829107e0f110a65715189986132ba020c9caf2c1
Lord Isfet <lordisfet@gmail.com>
2026-07-23 14:27:38 +0200

    Buzzer test

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,12 @@
 #define TAG_BUTTON "BUTTON"
 #define GPIO_BUTTON GPIO_NUM_16
 
-#define TAG_LED_ADC "LED_ADC"
-#define GPIO_LED_ADC GPIO_NUM_9
-#define ADC_CHANNEL_LED ADC1_GPIO9_CHANNEL
-#define TAG_LED "LED"
-#define GPIO_LED GPIO_NUM_21
-
-#define TAG_MOTOR_ADC "MOTOR_ADC"
-#define GPIO_MOTOR_ADC GPIO_NUM_4
-#define ADC_CHANNEL_MOTOR ADC1_GPIO4_CHANNEL
-#define TAG_MOTOR "MOTOR"
-#define GPIO_MOTOR GPIO_NUM_37
+#define TAG_BUZZER_ADC "BUZZER_ADC"
+#define GPIO_BUZZER_ADC GPIO_NUM_9
+#define ADC_CHANNEL_BUZZER ADC1_GPIO9_CHANNEL
+#define TAG_BUZZER "BUZZER"
+#define GPIO_BUZZER GPIO_NUM_21
+#define PWM_BUZZER_COEF 2./5
 
 #define ADC_UNIT ADC_UNIT_1
 #define ADC_READ_DELAY pdMS_TO_TICKS(20)
@@ -37,7 +32,7 @@
 #define ADC_MAX_RAW ((1 << ADC_BITWIDTH) - 1)
 #define ADC_ATTEN ADC_ATTEN_DB_11
 #define ADC_V_REF 3.3
-#define ADC_PATTERN_NUM 2
+#define ADC_PATTERN_NUM 1
 #define ADC_SAMPLE_FREQ_HZ 1000 * ADC_PATTERN_NUM
 #define ADC_RESULT_SET_SIZE 20
 #define ADC_FRAME_SET_SIZE 8
@@ -47,9 +42,6 @@
 #define PWM_FREQUENCY 2000
 #define PWM_MAX_DUTY ADC_MAX_RAW
 #define PWM_DUTY_CYCLE (uint16_t)(0 * PWM_MAX_DUTY)
- 
-#define V_TO_MV 1000
-#define SMA_SIZE 10
 
 void parse_adc_buffer(uint8_t *buffer, uint32_t lenght, adc_digi_output_data_t *raw, uint32_t *num_samples){
     uint32_t num = lenght / SOC_ADC_DIGI_RESULT_BYTES;
@@ -75,23 +67,15 @@ extern "C" void app_main()
 {
     Button button(TAG_BUTTON, GPIO_BUTTON);
 
-    gpio_config_t led_conf = {
-        .pin_bit_mask = (1ULL << GPIO_LED),
+    gpio_config_t buzzer_conf = {
+        .pin_bit_mask = (1ULL << GPIO_BUZZER),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE,
     };
-    ESP_ERROR_CHECK(gpio_config(&led_conf));
+    ESP_ERROR_CHECK(gpio_config(&buzzer_conf));
 
-    gpio_config_t motor_conf = {
-        .pin_bit_mask = (1ULL << GPIO_MOTOR),
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    ESP_ERROR_CHECK(gpio_config(&motor_conf));
 
     // --- ADC configuration and allocation --- 
     adc_continuous_handle_t adc_handle = nullptr;
@@ -104,13 +88,7 @@ extern "C" void app_main()
     adc_digi_pattern_config_t adc_patterns[ADC_PATTERN_NUM] = {
         {
             .atten = ADC_ATTEN,
-            .channel = ADC_CHANNEL_LED,
-            .unit = ADC_UNIT_1,
-            .bit_width = ADC_BITWIDTH
-        },
-        {
-            .atten = ADC_ATTEN,
-            .channel = ADC_CHANNEL_MOTOR,
+            .channel = ADC_CHANNEL_BUZZER,
             .unit = ADC_UNIT_1,
             .bit_width = ADC_BITWIDTH
         }
@@ -128,7 +106,7 @@ extern "C" void app_main()
 
 
     // --- PWM configuration and allocation ---
-    ledc_timer_config_t led_timer_config = {
+    ledc_timer_config_t pwm_timer_config = {
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .duty_resolution = LEDC_TIMER_12_BIT,
         .timer_num = LEDC_TIMER_0,
@@ -136,8 +114,8 @@ extern "C" void app_main()
         .clk_cfg = LEDC_USE_XTAL_CLK,
     };
 
-    ledc_channel_config_t led_channel_config = {
-        .gpio_num = GPIO_LED,
+    ledc_channel_config_t buzzer_channel_config = {
+        .gpio_num = GPIO_BUZZER,
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .channel = LEDC_CHANNEL_0,
         .intr_type = LEDC_INTR_DISABLE,
@@ -145,18 +123,8 @@ extern "C" void app_main()
         .duty = PWM_DUTY_CYCLE,
     };
 
-    ledc_channel_config_t motor_channel_config = {
-        .gpio_num = GPIO_MOTOR,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_1,
-        .intr_type = LEDC_INTR_DISABLE,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = PWM_DUTY_CYCLE,
-    };
-
-    ESP_ERROR_CHECK(ledc_timer_config(&led_timer_config));
-    ESP_ERROR_CHECK(ledc_channel_config(&led_channel_config));
-    ESP_ERROR_CHECK(ledc_channel_config(&motor_channel_config));
+    ESP_ERROR_CHECK(ledc_timer_config(&pwm_timer_config));
+    ESP_ERROR_CHECK(ledc_channel_config(&buzzer_channel_config));
 
     static adc_digi_output_data_t raw[ADC_RESULT_SET_SIZE];
     static uint8_t buffer[ADC_CONV_FRAME_SIZE] = {0};
@@ -170,28 +138,20 @@ extern "C" void app_main()
         button.update();
         if(button.isPressed())
         {
-            
             ledc_timer_resume(LEDC_LOW_SPEED_MODE, LEDC_TIMER_0);
             adc_continuous_read(adc_handle, buffer, ADC_CONV_FRAME_SIZE, &length, ADC_MAX_DELAY);
             parse_adc_buffer(buffer, length, raw, &samples);
 
-            avg_raw_data = calculate_avg_raw(raw, ADC_UNIT, ADC_CHANNEL_LED, samples);
-            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, avg_raw_data);
-            ESP_LOGI(TAG_LED_ADC, "ADC value: %lu, amount of measurements %lu", avg_raw_data, samples);
-            ESP_LOGI(TAG_LED, "PWM duty cycle: %lu", ledc_get_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+            avg_raw_data = calculate_avg_raw(raw, ADC_UNIT, ADC_CHANNEL_BUZZER, samples);
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, avg_raw_data * PWM_BUZZER_COEF);
+            ESP_LOGI(TAG_BUZZER_ADC, "ADC value: %lu, amount of measurements %lu", avg_raw_data, samples);
+            ESP_LOGI(TAG_BUZZER, "PWM duty cycle: %lu", ledc_get_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
             ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-
-            avg_raw_data = calculate_avg_raw(raw, ADC_UNIT, ADC_CHANNEL_MOTOR, samples);
-            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, avg_raw_data);
-            ESP_LOGI(TAG_MOTOR_ADC, "ADC value: %lu, amount of measurements %lu", avg_raw_data, samples);
-            ESP_LOGI(TAG_LED, "PWM duty cycle: %lu", ledc_get_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
-            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
         }
         else
         {
             ledc_timer_pause(LEDC_LOW_SPEED_MODE, LEDC_TIMER_0);
-            gpio_set_level(GPIO_LED, 0);
-            gpio_set_level(GPIO_MOTOR, 0);
+            gpio_set_level(GPIO_BUZZER, 0);
         }
 
         vTaskDelay(ADC_READ_DELAY);
