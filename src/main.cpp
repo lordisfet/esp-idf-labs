a2544529d8483467a16660bb487e3612402815e9
Lord Isfet <lordisfet@gmail.com>
2026-08-22 18:16:20 +0200

    Add i2c bus class

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <u8x8.h>
 
 #include "menu/Menu.h"
+#include "i2c_bus/I2CBus.h"
 
 #define TASK_DELAY_TICKS pdMS_TO_TICKS(1000)
 #define SDA_PIN GPIO_NUM_15
@@ -16,104 +17,16 @@
 #define SSD1306_HEIGHT 64
 #define SSD1306_WIDTH 128
 
-void i2c_master_init()
-{
-    i2c_config_t conf = {}; 
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = SDA_PIN;
-    conf.scl_io_num = SCL_PIN;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = 400000;
-
-    i2c_param_config(I2C_PORT, &conf);
-    i2c_driver_install(I2C_PORT, conf.mode, 0, 0, 0);
-}
-
-uint8_t write_byte(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
-    // U8X8_MSG_BYTE_INIT
-    // U8X8_MSG_BYTE_SEND 30
-    // U8X8_MSG_BYTE_SET_DC 31
-    // U8X8_MSG_BYTE_START_TRANSFER
-    // U8X8_MSG_BYTE_END_TRANSFER
-    // U8X8_MSG_BYTE_SET_I2C_ADR (obsolete)
-    // U8X8_MSG_BYTE_SET_DEVICE (obsolete)
-    static i2c_cmd_handle_t handle;
-    switch (msg)
-    {
-    case U8X8_MSG_BYTE_INIT:
-        /* code */
-        break;
-    case U8X8_MSG_BYTE_START_TRANSFER: {
-        handle = i2c_cmd_link_create();
-
-        i2c_master_start(handle);
-
-        uint8_t i2c_address = u8x8_GetI2CAddress(u8x8); 
-        i2c_master_write_byte(handle, i2c_address | I2C_MASTER_WRITE, true);
-        break;
-    }
-    case U8X8_MSG_BYTE_SEND:
-        // 4. Добавляем в очередь массив байт (картинку или команды)
-        // arg_ptr - это указатель на данные, arg_int - их длина.
-        i2c_master_write(handle, (uint8_t *)arg_ptr, arg_int, true);
-        break;
-    case U8X8_MSG_BYTE_END_TRANSFER:
-        // 5. Добавляем сигнал STOP
-        i2c_master_stop(handle);
-        
-        // 6. Выполняем всю собранную очередь физически! Ждем ответа до 1000 мс.
-        i2c_master_cmd_begin(I2C_PORT, handle, pdMS_TO_TICKS(1000));
-        
-        // 7. Уничтожаем очередь, освобождая память
-        i2c_cmd_link_delete(handle);
-    break;
-    default:
-         break;
-    }
-
-    return 1;
-}
-
-uint8_t gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
-    // U8X8_MSG_GPIO_INIT
-    // U8X8_MSG_DELAY_MILLI
-    // U8X8_MSG_DELAY_10MICRO
-    // U8X8_MSG_DELAY_100NANO
-    // U8X8_MSG_DELAY_NANO
-    switch (msg)
-    {
-    case U8X8_MSG_GPIO_AND_DELAY_INIT:
-        /* code */
-        break;
-    case U8X8_MSG_DELAY_MILLI:
-        vTaskDelay(pdMS_TO_TICKS(arg_int));
-        break;
-    case U8X8_MSG_DELAY_10MICRO:
-        esp_rom_delay_us(10);
-        break;
-    case U8X8_MSG_DELAY_100NANO:
-        esp_rom_delay_us(1);
-        break;    
-    case U8X8_MSG_DELAY_NANO:
-        /* code */
-        break;
-    default:
-        break;
-    }
-
-    return 1;
-}
-
 extern "C" void app_main(void)
 {
-    i2c_master_init();
+    I2CBus i2c_bus(I2C_PORT, I2C_MODE_MASTER, SDA_PIN, SCL_PIN);
+    i2c_bus.scan();
 
     RTC rtc;
     rtc.init();
 
     Display display;
-    
+
     ScreenLayout layout = {
         .time = {10, 35, u8g2_font_logisoso24_tn},
         .dotw = {20,  55, u8g2_font_profont12_tf},
